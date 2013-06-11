e3ec0b97e2d45c8c335b70b7dd5ce1eca6a396fd
unknown <sam.birkhead@44397-WEB-WIN.auckland.natcoll.ac.nz>
2013-06-11 14:31:31 +1200

    commit1

---
+++ includes/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+  <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>Template Page</title>
+      <link href="assets/styles.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+
+<div id="container">
+
+
+	<div id="header">
+
+		<div id="logo">
+
+		</div>
+
+
+		<ul id="productNav">
+
+			<li id="tees">
+				<a href="#">TEES</a>
+			</li>
+
+			<li id="singlets">
+				<a href="#">SINGLETS</a>
+			</li>
+
+			<li id="hoodies">
+				<a href="#">HOODIES</a>
+			</li>
+
+		</ul>
+
+		<ul id="userNav">
+
+			<li id="loginRegister">
+				<a href="#">LOGIN / REGISTER</a>
+			</li>
+
+			<li id="cart">
+				<a href="#">CART</a>
+			</li>
+
+		</ul>
+
+	</div>
+
+
+	<div id="content">
---
+++ products.php
@@ -0,0 +1,39 @@
+<?php
+	require_once("includes/header.php");
+?>
+
+		<ul id="teePageCatalogue">
+
+			<li>
+				<a href="#"> 
+					<img src="/assets/images/tee_basic.jpg">
+					<h2>BASIC TEE</h2>
+				</a>
+				$25.00<br/>
+				<a href="">Add to Cart</a>
+			</li>
+
+			<li>
+				<a href="#"> 
+					<img src="/assets/images/tee_pocket.jpg">
+				</a>
+			</li>
+
+			<li>
+				<a href="#"> 
+					<img src="/assets/images/tee_scoop.jpg">
+				</a>
+			</li>
+
+			<li>
+				<a href="#"> 
+					<img src="/assets/images/tee_vee.jpg">
+				</a>
+			</li>
+
+		</ul>
+
+		
+<?php
+	require_once("includes/footer.php");
+?>
